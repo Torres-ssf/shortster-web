cf0b50a6c6f9bc1d4364466f227151f69575484d
Torres-ssf <sergio.uft@gmail.com>
2020-12-13 19:55:38 -0300

    feat(mainpage): implement main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container } from './styles';
+import { Input } from '../../components/Input';
+
+import { Container, InputContainer } from './styles';
 
 export const Main: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <Container>
-      <h1>Hello World</h1>
+      <h1>Shortster</h1>
+
+      <p>Paste a URL to create a new shortster</p>
+
+      <InputContainer>
+        <Input
+          name="url"
+          type="url"
+          placeholder="Paste a URL"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <button type="button">Create shortster</button>
+      </InputContainer>
     </Container>
   );
 };
