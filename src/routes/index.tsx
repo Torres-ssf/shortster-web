06a56553c8c9f2a6e1ff05212e2fd0ad02679ecf
Torres-ssf <sergio.uft@gmail.com>
2020-12-14 18:22:09 -0300

    feat(app routes): add Redirect page route to app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Main } from '../pages/Main';
+import { Redirect } from '../pages/Redirect';
 
 export const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" component={Main} />
+    <Route path="/" exact component={Main} />
+
+    <Route path="/:code" component={Redirect} />
   </Switch>
 );
