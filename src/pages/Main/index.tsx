35a9e18f9bfd4ccbbba68b251331f0722b1c9e25
Torres-ssf <sergio.uft@gmail.com>
2020-12-14 17:00:01 -0300

    feat(main page): display created shorster

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 
 import { Input } from '../../components/Input';
@@ -6,16 +7,33 @@ import { Button } from '../../components/Button';
 import { api } from '../../services/api';
 import { getValidationErrors } from '../../util/getValidationErrors';
 
-import { Container, InputContainer, ErrorList } from './styles';
+import {
+  Container,
+  InputContainer,
+  ErrorList,
+  ShortsterContainer,
+} from './styles';
+
+interface Shortster {
+  code: string | undefined;
+  url: string | undefined;
+}
 
 export const Main: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const [createdShorster, setCreatedShortster] = useState<Shortster>(
+    {} as Shortster,
+  );
+
   const [loading, setLoading] = useState(false);
 
   const [errorMessages, setErrorMessages] = useState<String[]>([]);
 
   const handleCreateShortster = useCallback(async () => {
     setLoading(true);
+
+    setCreatedShortster({ code: undefined, url: undefined });
 
     let url = inputValue.toLowerCase();
 
@@ -38,30 +56,41 @@ export const Main: React.FC = () => {
 
       const { data } = await api.post('/shortster', { url });
 
-      console.log(data);
+      setCreatedShortster({
+        code: data.code,
+        url: data.url,
+      });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
 
         setErrorMessages(Object.values(errors));
       }
+
+      if (err.response && err.response.data) {
+        const { message } = err.response.data;
+
+        setErrorMessages(
+          typeof message === 'string' ? [message] : [...message],
+        );
+      }
     } finally {
       setLoading(false);
     }
   }, [inputValue]);
+
+  const { code, url } = useMemo(() => {
+    return createdShorster;
+  }, [createdShorster]);
 
   const errorList = useMemo(() => {
     if (errorMessages.length === 0) {
       return <></>;
     }
 
-    return (
-      <ErrorList>
-        {errorMessages.map(message => {
-          return <li>{message}</li>;
-        })}
-      </ErrorList>
-    );
+    return errorMessages.map(message => {
+      return <li>{message}</li>;
+    });
   }, [errorMessages]);
 
   return (
@@ -83,7 +112,22 @@ export const Main: React.FC = () => {
           Create shortster
         </Button>
       </InputContainer>
-      {errorList}
+
+      <ErrorList>{errorList}</ErrorList>
+
+      {code && (
+        <ShortsterContainer>
+          <h3>Shortster successfully created</h3>
+          <span>
+            Shortster:
+            <Link to="/code">{`shorster/${code}`}</Link>
+          </span>
+          <span>
+            Long url:
+            <Link to="/code">{`${url}`}</Link>
+          </span>
+        </ShortsterContainer>
+      )}
     </Container>
   );
 };
