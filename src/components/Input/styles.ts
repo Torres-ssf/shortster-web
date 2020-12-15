99bd07a812895e610384fd813c0455414fed7ef2
Torres-ssf <sergio.uft@gmail.com>
2020-12-14 22:50:45 -0300

    style(input component): update styles from Input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,23 +1,22 @@
 import styled, { css } from 'styled-components';
 
+import { Tooltip } from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
   background: #fff;
-  border-radius: 10px 0 0 10px;
+  border-radius: 10px;
   border: 1px solid #5171a5;
   display: flex;
   align-items: center;
   padding: 16px;
   height: 60px;
   width: 100%;
-
-  & + div {
-    margin-top: 8px;
-  }
 
   ${props =>
     props.isFocused &&
@@ -30,6 +29,12 @@ export const Container = styled.div<ContainerProps>`
     css`
       border: 2px solid #3866ff;
     `}
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border: 2px solid #c53030;
+    `}
     
     input {
     flex: 1;
@@ -38,6 +43,24 @@ export const Container = styled.div<ContainerProps>`
 
     &::placeholder {
       color: #a6a3a0;
+    }
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  text-align: center;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
     }
   }
 `;
